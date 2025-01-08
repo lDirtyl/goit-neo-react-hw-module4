91eedb984e1a302ef912565a2d971210cba5085c
Andrii Mashtaler <andrii.mashtaler@gmx.at>
2025-01-08 01:38:01 +0100

    Struktur

---
+++ src/components/ImageModal/ImageModal.jsx
@@ -0,0 +1,7 @@
+const ImageModal = () => {
+    return(
+        <div></div>
+    )
+}
+
+export default ImageModal;
---
+++ src/components/LoadMoreBtn/LoadMoreBtn.jsx
@@ -0,0 +1,7 @@
+const LoadMoreBtn = () => {
+    return(
+        <div></div>
+    )
+}
+
+export default LoadMoreBtn;

b3fab7dfa368719b46393809f9c0ae4c29890057
Andrii Mashtaler <andrii.mashtaler@gmx.at>
2025-01-08 01:48:43 +0100

    Api.js acces KEY

---
+++ src/Api/Api.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://api.unsplash.com/';
+
+axios.defaults.headers['Authorization'] =
+  'Client-ID 8wPQUsYRAEslZL8o2RdsRXiY60r6FJVWq9Od5sq9vU4';

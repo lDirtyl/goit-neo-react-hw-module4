77084f4ec1536c90557b9db5f7619b1a300914d3
Andrii Mashtaler <andrii.mashtaler@gmx.at>
2025-01-08 02:33:42 +0100

    ImageModal finished

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -1,7 +1,23 @@
-const ImageModal = () => {
-    return(
-        <div></div>
-    )
-}
+import Modal from 'react-modal';
 
-export default ImageModal;
+Modal.setAppElement('#root');
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    backgroundColor: 'transparent',
+  },
+};
+
+export default function ImageModal({ isOpen, onClose, src, alt }) {
+  return (
+    <Modal isOpen={isOpen} onRequestClose={onClose} style={customStyles}>
+      <img src={src} alt={alt} />
+    </Modal>
+  );
+}

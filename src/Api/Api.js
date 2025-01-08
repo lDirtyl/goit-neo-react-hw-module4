d67bf4ed3306c485baacdcc3249a55be1448b30b
Andrii Mashtaler <andrii.mashtaler@gmx.at>
2025-01-08 02:39:04 +0100

    Api getPhotos

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -4,3 +4,8 @@ axios.defaults.baseURL = 'https://api.unsplash.com/';
 
 axios.defaults.headers['Authorization'] =
   'Client-ID 8wPQUsYRAEslZL8o2RdsRXiY60r6FJVWq9Od5sq9vU4';
+
+  export const getPhotos = async (name, page = 1) => {
+    const response = await axios.get(`/search/photos?query=${name}&page=${page}`);
+    return response.data;
+  };

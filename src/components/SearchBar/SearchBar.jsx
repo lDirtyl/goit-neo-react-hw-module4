5025b4b0492407a21b08fb15971d042adc32f944
Andrii Mashtaler <andrii.mashtaler@gmx.at>
2025-01-08 02:00:44 +0100

    SearchBar finish

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,7 +1,28 @@
-const SearchBar = () => {
-    return(
-        <div></div>
-    )
-}
+import toast from "react-hot-toast";
 
-export default SearchBar;
+
+export default function SearchBar ({onSubmit})  {
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        const value = e.target.elements.query.value
+        if (!value) {
+            toast.error('⚠️ Please provide an image name!')
+        } else {
+            onSubmit(value)
+        }
+    }
+
+  return (
+    <header>
+      <form onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+        <button type="submit">Search</button>
+      </form>
+    </header>
+  );
+};
